b75cbcda6d79609acf98742ed41777edea9a00c9
AravindhanK07 <149782911+AravindhanK07@users.noreply.github.com>
2024-05-31 09:50:38 +0530

    Add client and server folders

---
+++ server/src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+//Middle ware
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["https://deploy-mern-1whq.versel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://AravindhanAK:Aravindhan003@recipes.2tcrgiz.mongodb.net/recipes?retryWrites=true&w=majority&appName=recipes"
+);
+
+app.listen(3001, () => console.log("server started"));
